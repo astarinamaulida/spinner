2dfdcec50e637e9002b64900dcd5be894b289d19
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-22 04:48:58 -0400

    Refactor spinner1

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const spinner = function (circle, timeout) {
+  const lines = ['\r|   ', '\r/   ', '\r-   ', '\r|   '];
+  for (let i = 0; i < circle; i++)
+    for (const line of lines) {
+      setTimeout(() => {
+        process.stdout.write(line);
+      }, timeout);
+      timeout += 200;
+    }
+}
+
+// to make it 4 cycles
+spinner(4, 0);
